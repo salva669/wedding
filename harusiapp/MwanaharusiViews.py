75aa5e9ad0b81eeb85c318a4180ace47b09983cf
salva66 <henrymkama@gmail.com>
2025-09-12 18:32:41 +0300

    sept4

---
+++ harusiapp/MwanaharusiViews.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render
+import datetime
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from schoolapp.models import Students, Courses, Subjects, CustomUser, LeaveReportStudent, FeedBackStudent, NotificationStudent, StudentResult
+
+def student_home(request):
+    student_obj=Students.objects.get(admin=request.user.id)
+    course=Courses.objects.get(id=student_obj.course_id.id)
+    subjects=Subjects.objects.filter(course_id=course).count()
+    subjects_data=Subjects.objects.filter(course_id=course)
+    
+    subject_name=[]
+    subject_data=Subjects.objects.filter(course_id=student_obj.course_id)
+    for subject in subject_data:
+        subject_name.append(subject.subject_name)
+
+    return render(request,"student_template/student_home_template.html",{"subjects":subjects,"data_name":subject_name})
+
+def student_apply_leave(request):
+    staff_obj = Students.objects.get(admin=request.user.id)
+    leave_data=LeaveReportStudent.objects.filter(student_id=staff_obj)
+    return render(request,"student_template/student_apply_leave.html",{"leave_data":leave_data})
+
+def student_apply_leave_save(request):
+    if request.method!="POST":
+        return HttpResponseRedirect(reverse("student_apply_leave"))
+    else:
+        leave_date=request.POST.get("leave_date")
+        leave_msg=request.POST.get("leave_msg")
+
+        student_obj=Students.objects.get(admin=request.user.id)
+        try:
+            leave_report=LeaveReportStudent(student_id=student_obj,leave_date=leave_date,leave_message=leave_msg,leave_status=0)
+            leave_report.save()
+            messages.success(request, "Successfully Applied for Leave")
+            return HttpResponseRedirect(reverse("student_apply_leave"))
+        except:
+            messages.error(request, "Failed To Apply for Leave")
+            return HttpResponseRedirect(reverse("student_apply_leave"))
+
+
+def student_feedback(request):
+    staff_id=Students.objects.get(admin=request.user.id)
+    feedback_data=FeedBackStudent.objects.filter(student_id=staff_id)
+    return render(request,"student_template/student_feedback.html",{"feedback_data":feedback_data})
+
+def student_feedback_save(request):
+    if request.method!="POST":
+        return HttpResponseRedirect(reverse("student_feedback"))
+    else:
+        feedback_msg=request.POST.get("feedback_msg")
+
+        student_obj=Students.objects.get(admin=request.user.id)
+        try:
+            feedback=FeedBackStudent(student_id=student_obj,feedback=feedback_msg,feedback_reply="")
+            feedback.save()
+            messages.success(request, "Successfully Sent Feedback")
+            return HttpResponseRedirect(reverse("student_feedback"))
+        except:
+            messages.error(request, "Failed To Send Feedback")
+            return HttpResponseRedirect(reverse("student_feedback"))
+
+def student_profile(request):
+    user=CustomUser.objects.get(id=request.user.id)
+    student=Students.objects.get(admin=user)
+    return render(request,"student_template/student_profile.html",{"user":user,"student":student})
+
+def student_profile_save(request):
+    if request.method!="POST":
+        return HttpResponseRedirect(reverse("student_profile"))
+    else:
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        password=request.POST.get("password")
+        address=request.POST.get("address")
+        try:
+            customuser=CustomUser.objects.get(id=request.user.id)
+            customuser.first_name=first_name
+            customuser.last_name=last_name
+            if password!=None and password!="":
+                customuser.set_password(password)
+            customuser.save()
+
+            student=Students.objects.get(admin=customuser)
+            student.address=address
+            student.save()
+            messages.success(request, "Successfully Updated Profile")
+            return HttpResponseRedirect(reverse("student_profile"))
+        except:
+            messages.error(request, "Failed to Update Profile")
+            return HttpResponseRedirect(reverse("student_profile"))
---
+++ harusiapp/StaffViews.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render
+import json
+from datetime import datetime
+from uuid import uuid4
+from django.contrib import messages
+from django.core import serializers
+from django.forms import model_to_dict
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from schoolapp.models import Subjects, Mwanaharusis, LeaveReportStaff, Staffs, FeedBackStaffs, CustomUser, Courses, NotificationStaffs
+
+def staff_home(request):
+    #For Fetch All Mwanaharusi Under Staff
+    subjects=Subjects.objects.filter(staff_id=request.user.id)
+    course_id_list=[]
+    for subject in subjects:
+        course=Courses.objects.get(id=subject.course_id.id)
+        course_id_list.append(course.id)
+
+    final_course=[]
+    #removing Duplicate Course ID
+    for course_id in course_id_list:
+        if course_id not in final_course:
+            final_course.append(course_id)
+
+    mwanaharusis_count=Mwanaharusis.objects.filter(course_id__in=final_course).count()
+
+    #Fetch All Approve Leave
+    staff=Staffs.objects.get(admin=request.user.id)
+    leave_count=LeaveReportStaff.objects.filter(staff_id=staff.id,leave_status=1).count()
+    subject_count=subjects.count()
+
+    return render(request,"staff_template/staff_home_template.html",{"mwanaharusis_count":mwanaharusis_count,"leave_count":leave_count,"subject_count":subject_count})
+
+def staff_apply_leave(request):
+    staff_obj = Staffs.objects.get(admin=request.user.id)
+    leave_data=LeaveReportStaff.objects.filter(staff_id=staff_obj)
+    return render(request,"staff_template/staff_apply_leave.html",{"leave_data":leave_data})
+
+def staff_apply_leave_save(request):
+    if request.method!="POST":
+        return HttpResponseRedirect(reverse("staff_apply_leave"))
+    else:
+        leave_date=request.POST.get("leave_date")
+        leave_msg=request.POST.get("leave_msg")
+
+        staff_obj=Staffs.objects.get(admin=request.user.id)
+        try:
+            leave_report=LeaveReportStaff(staff_id=staff_obj,leave_date=leave_date,leave_message=leave_msg,leave_status=0)
+            leave_report.save()
+            messages.success(request, "Successfully Applied for Leave")
+            return HttpResponseRedirect(reverse("staff_apply_leave"))
+        except:
+            messages.error(request, "Failed To Apply for Leave")
+            return HttpResponseRedirect(reverse("staff_apply_leave"))
+
+
+def staff_feedback(request):
+    staff_id=Staffs.objects.get(admin=request.user.id)
+    feedback_data=FeedBackStaffs.objects.filter(staff_id=staff_id)
+    return render(request,"staff_template/staff_feedback.html",{"feedback_data":feedback_data})
+
+def staff_feedback_save(request):
+    if request.method!="POST":
+        return HttpResponseRedirect(reverse("staff_feedback_save"))
+    else:
+        feedback_msg=request.POST.get("feedback_msg")
+
+        staff_obj=Staffs.objects.get(admin=request.user.id)
+        try:
+            feedback=FeedBackStaffs(staff_id=staff_obj,feedback=feedback_msg,feedback_reply="")
+            feedback.save()
+            messages.success(request, "Successfully Sent Feedback")
+            return HttpResponseRedirect(reverse("staff_feedback"))
+        except:
+            messages.error(request, "Failed To Send Feedback")
+            return HttpResponseRedirect(reverse("staff_feedback"))
+
+def staff_profile(request):
+    user=CustomUser.objects.get(id=request.user.id)
+    staff=Staffs.objects.get(admin=user)
+    return render(request,"staff_template/staff_profile.html",{"user":user,"staff":staff})
+
+def staff_profile_save(request):
+    if request.method!="POST":
+        return HttpResponseRedirect(reverse("staff_profile"))
+    else:
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        address=request.POST.get("address")
+        password=request.POST.get("password")
+        try:
+            customuser=CustomUser.objects.get(id=request.user.id)
+            customuser.first_name=first_name
+            customuser.last_name=last_name
+            if password!=None and password!="":
+                customuser.set_password(password)
+            customuser.save()
+
+            staff=Staffs.objects.get(admin=customuser.id)
+            staff.address=address
+            staff.save()
+            messages.success(request, "Successfully Updated Profile")
+            return HttpResponseRedirect(reverse("staff_profile"))
+        except:
+            messages.error(request, "Failed to Update Profile")
+            return HttpResponseRedirect(reverse("staff_profile"))
